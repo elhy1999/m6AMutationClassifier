4276c10f830b8fb15b9923341a8219c631605f2f
elhy1999 <ernestliu64@gmail.com>
2023-11-12 17:54:10 +0800

    Modify data_preparation.R with correct file paths

--- src/data_preparation.R
+++ src/data_preparation.R
@@ -3,20 +3,23 @@ library(ggplot2)
 library(dplyr)
 
 # Read prediction CSV files for different cell lines and replicates
-a549_1 <- read.csv('Predicted/SGNex_A549_directRNA_replicate5_run1_predictions.csv')
-a549_2 <- read.csv('Predicted/SGNex_A549_directRNA_replicate6_run1_predictions.csv')
-hct116_1 <- read.csv('Predicted/SGNex_Hct116_directRNA_replicate3_run1_predictions.csv')
-hct116_2 <- read.csv('Predicted/SGNex_Hct116_directRNA_replicate3_run4_predictions.csv')
-hct116_3 <- read.csv('Predicted/SGNex_Hct116_directRNA_replicate4_run3_predictions.csv')
-hepg2_1 <- read.csv('Predicted/SGNex_HepG2_directRNA_replicate5_run2_predictions.csv')
-hepg2_2 <- read.csv('Predicted/SGNex_HepG2_directRNA_replicate6_run1_predictions.csv')
-k562_1 <- read.csv('Predicted/SGNex_K562_directRNA_replicate4_run1_predictions.csv')
-k562_2 <- read.csv('Predicted/SGNex_K562_directRNA_replicate5_run1_predictions.csv')
-k562_3 <- read.csv('Predicted/SGNex_K562_directRNA_replicate6_run1_predictions.csv')
-mcf7_1 <- read.csv('Predicted/SGNex_MCF7_directRNA_replicate3_run1_predictions.csv')
-mcf7_2 <- read.csv('Predicted/SGNex_MCF7_directRNA_replicate4_run1_predictions.csv')
+print("Reading data files...")
+a549_1 <- read.csv('./../data/curated/SGNex_A549_directRNA_replicate5_run1_predictions.csv')
+a549_2 <- read.csv('./../data/curated/SGNex_A549_directRNA_replicate6_run1_predictions.csv')
+hct116_1 <- read.csv('./../data/curated/SGNex_Hct116_directRNA_replicate3_run1_predictions.csv')
+hct116_2 <- read.csv('./../data/curated/SGNex_Hct116_directRNA_replicate3_run4_predictions.csv')
+hct116_3 <- read.csv('./../data/curated/SGNex_Hct116_directRNA_replicate4_run3_predictions.csv')
+hepg2_1 <- read.csv('./../data/curated/SGNex_HepG2_directRNA_replicate5_run2_predictions.csv')
+hepg2_2 <- read.csv('./../data/curated/SGNex_HepG2_directRNA_replicate6_run1_predictions.csv')
+k562_1 <- read.csv('./../data/curated/SGNex_K562_directRNA_replicate4_run1_predictions.csv')
+k562_2 <- read.csv('./../data/curated/SGNex_K562_directRNA_replicate5_run1_predictions.csv')
+k562_3 <- read.csv('./../data/curated/SGNex_K562_directRNA_replicate6_run1_predictions.csv')
+mcf7_1 <- read.csv('./../data/curated/SGNex_MCF7_directRNA_replicate3_run1_predictions.csv')
+mcf7_2 <- read.csv('./../data/curated/SGNex_MCF7_directRNA_replicate4_run1_predictions.csv')
+print("Data files read!")
 
 # Rename the 'score' column in each data frame
+print("Renaming columns...")
 a549_1 <- a549_1 %>% rename(a549_1 = score)
 a549_2 <- a549_2 %>% rename(a549_2 = score)
 hct116_1 <- hct116_1 %>% rename(hct116_1 = score)
@@ -29,8 +32,10 @@ k562_2 <- k562_2 %>% rename(k562_2 = score)
 k562_3 <- k562_3 %>% rename(k562_3 = score)
 mcf7_1 <- mcf7_1 %>% rename(mcf7_1 = score)
 mcf7_2 <- mcf7_2 %>% rename(mcf7_2 = score)
+print("Columns renamed!")
 
 # Remove words after the decimal point in the 'transcript_id' column of mcf7_1
+print("Preparing all dataframes...")
 mcf7_1$transcript_id <- sub("\\..*$", "", mcf7_1$transcript_id)
 
 # Concatenate 'transcript_id' and 'transcript_position' columns into a new column with "|" separator
@@ -58,13 +63,16 @@ all_transcript_id_positions <- bind_rows(
   a549_1, a549_2, hct116_1, hct116_2, hct116_3, hepg2_1, hepg2_2,
   k562_1, k562_2, k562_3, mcf7_1, mcf7_2
 )
+print("Dataframes prepared!")
 
 # Get unique values from the combined data frame
+print("Getting the unique transcript IDs and positions...")
 unique_transcript_id_positions <- unique(all_transcript_id_positions)
 pri_key <- data.frame(transcript_id_position = unique_transcript_id_positions)
 
 # Merge data frames with unique values to create a single data frame
 # containing all the columns
+print("Joining all datasets together...")
 pri_key <- merge(pri_key, a549_1, by='transcript_id_position', all=T)
 pri_key <- merge(pri_key, a549_2, by='transcript_id_position', all=T)
 pri_key <- merge(pri_key, hct116_1, by='transcript_id_position', all=T)
@@ -77,8 +85,10 @@ pri_key <- merge(pri_key, k562_2, by='transcript_id_position', all=T)
 pri_key <- merge(pri_key, k562_3, by='transcript_id_position', all=T)
 pri_key <- merge(pri_key, mcf7_1, by='transcript_id_position', all=T)
 pri_key <- merge(pri_key, mcf7_2, by='transcript_id_position', all=T)
+print("Datasets joined!")
 
 # Calculate row sums
+print("Calculating rowwise sums...")
 row_sums <- rowSums(pri_key[, -1])
 
 # Combine row sums with the original data frame
@@ -95,12 +105,17 @@ binary_pri_key <- pri_key %>%
 binary_row_sums <- rowSums(binary_pri_key[, -1])
 binary_pri_sums <- cbind(binary_pri_key, binary_row_sums)
 sorted_binary_key <- binary_pri_sums[order(-binary_row_sums), ]
+print("Calculations completed!")
 
 # Filter for rows with row sums equal to 12
+print("Filtering for significant transcripts...")
 filtered_df <- binary_pri_sums %>%
   filter(binary_row_sums == 12)
+print("Filtered!")
 
 # Export the filtered data frame as a CSV file in the current folder
+print("Writing result to disk...")
 current_dir <- getwd()
 filepath <- file.path(current_dir, "fully_mutated.csv")
 write.csv(filtered_df, file=filepath, row.names = F)
+print(paste("Results written to:", filepath))
